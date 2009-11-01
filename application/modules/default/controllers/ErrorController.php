85379f1278a301b2eb3cf83e87841f02db6ee52c
Benjamin "balupton" Lupton <contact@balupton.com>
2009-11-01 21:33:21 +0000

    more error improvement (incorporated gates errorcontroller)

--- application/modules/default/controllers/ErrorController.php
+++ application/modules/default/controllers/ErrorController.php
@@ -1,33 +1,102 @@
 <?php
 
-class ErrorController extends Zend_Controller_Action
-{
-
-	public function init () {
-		// Layout
-		$this->getHelper('Layout')->setLayout('error');
-	}
+class ErrorController extends Zend_Controller_Action {
 	
-    public function errorAction() {
-        $errors = $this->_getParam('error_handler');
+	/**
+	 * Initialise our Zend Controller
+	 * @return
+	 */
+    public function init() {
+    	// Initialise Ajaxy Helper
+		// $this->getHelper('Ajaxy')->configure($this, $this->view, $this->getRequest());
+		$this->getHelper('Layout')->setLayout('error');
+    }
 
+
+	# ========================
+	# ACTIONS
+	
+	/**
+	 * Fired when an error occurs
+	 * @return
+	 */
+    public function errorAction() {
+    	// Fetch
+        $errors = $this->_getParam('error_handler');
+		$Exception = $errors->exception;
+		$Request = $errors->request;
+		$messages = array();
+		
+		// Determine
 		switch ($errors->type) {
 		    case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
 		    case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
-		
-		        // 404 error -- controller or action not found
+				// 404 error -- controller or action not found
 		        $this->getResponse()->setHttpResponseCode(404);
-		        $this->view->message = 'Page not found';
+		        $this->view->error = 'error-404';
 		        break;
+			
+			case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_OTHER:
+				// Thrown but unkown, perhaps managed?
+				// Check if doctrine
+				$class = get_class($Exception);
+				switch ( $class ) {
+					case 'Doctrine_Validator_Exception':
+						$this->view->error = 'error-doctrine-validation';
+						// Fetch
+						$invalidRecords = $Exception->getInvalidRecords();
+						// Cycle
+						foreach ( $invalidRecords as $Record ) {
+							// Fetch
+							$ErrorStack = $Record->getErrorStack()->toArray();
+							// Cycle
+							foreach ( $ErrorStack as $field => $errors ) {
+								foreach ( $errors as $error ) {
+									// Prepare
+									$Table = $Record->getTable();
+									$message = array(
+										'code' => 'error-doctrine-validation-'.$error,
+										'table' => strtolower($Table->getComponentName()),
+										'field' => $field,
+										'value' => $Record->get($field)
+									);
+									// Handle
+									switch ( $error ) {
+										case 'type':
+											$type = $Table->getTypeOf($field);
+											$message['type'] = $type;
+											break;
+										case 'length':
+											$properties = $Table->getDefinitionOf($field);
+											$message['length'] = $properties['length'];
+										default:
+											break;
+									}
+									// Append
+									$messages[] = $message;
+								}
+							}
+						}
+						break;
+					default:
+						break;
+				}
+				
 		    default:
 		        // application error
 		        $this->getResponse()->setHttpResponseCode(500);
-		        $this->view->message = 'Application error';
+				if ( empty($this->view->error) ) $this->view->error = 'error-application';
 		        break;
 		}
 		
-		$this->view->exception = $errors->exception;
-		$this->view->request   = $errors->request;
+		// Assign
+		$this->view->exception 	= $Exception;
+		$this->view->request   	= $Request;
+		$this->view->messages	= $messages;
+		
+		// Render
+		$this->view->headTitle()->append('Error');
+		$this->view->page = 'error';
 		
 		// Log the error
 		$Log = Zend_Registry::get('Log');
