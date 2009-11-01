540482fa886dc53c45b4e41c51c3b847d1ad3030
Benjamin "balupton" Lupton <contact@balupton.com>
2009-11-01 20:29:45 +0000

    edit page is working except for tags

--- public/back/scripts/cms.js
+++ public/back/scripts/cms.js
@@ -1,4 +1,19 @@
 (function(){
+	// By Benjamin "balupton" Lupton (MIT Licenced) - unless specified otherwise
+	
+	// Debug
+	if (typeof console === 'undefined') {
+		console = typeof window.console !== 'undefined' ? window.console : {};
+	}
+	console.log			= console.log 			|| function(){};
+	console.debug		= console.debug 		|| console.log;
+	console.warn		= console.warn			|| console.log;
+	console.error		= console.error			|| function(){var args = [];for (var i = 0; i < arguments.length; i++) { args.push(arguments[i]); } alert(args.join("\n")); };
+	console.trace		= console.trace			|| console.log;
+	console.group		= console.group			|| console.log;
+	console.groupEnd	= console.groupEnd		|| console.log;
+	console.profile		= console.profile		|| console.log;
+	console.profileEnd	= console.profileEnd	|| console.log;
 	
 	// Core Prototypes
 	String.prototype.wrap = function(start,end){
@@ -30,6 +45,10 @@
 
 	
 	// jQuery Prototypes
+	$.fn.inDOM = function(){
+		var $ancestor = $(this).parent().parent();
+		return $ancestor.size() && ($ancestor.height()||$ancestor.width());
+	};
 	$.fn.valWrap = function(start,end){
 		var $field = $(this);
 		return $field.val($field.val().wrap(start,end));
@@ -78,8 +97,6 @@
 	$.fn.firstInput = function(){
 		return $(this).findAndSelf(':input:first');
 	}
-	
-	// Helpers
 	$.fn.cancel = function(callback){
 		return $(this).findAndSelf(':input').keypress(function(e){
 			if ( e.keyCode === 27 ) { // ESC
@@ -103,176 +120,8 @@
 		return val;
 	}
 	
-	// checklist
-	if ( false ) $.fn.checklist = function(){
-		// Prepare
-		var options = {};
-		var defaults = {
-			list: '<span class="checklist">',
-			item: '<span class="checklist-item">',
-			title: '<span class="checklist-title">',
-			remove_button: '<a class="checklist-delete">x</a>',
-			remove: function($select,e){
-				var $remove_button = $(this);
-				var $item = $remove_button.parent();
-				var value = $item.data('value');
-				$select.children('option[value='+value+']').removeAttr('selected');
-				return true;
-			},
-			clear: '<span class="clear"></span>',
-			hideClass: 'hide',
-			add_button: '',
-			add_field: '',
-			add: function(){
-			}
-		}
-		if ( typeof arguments[0] === 'string' ) {
-			options = {
-				edit: arguments[0]
-			};
-		} else if ( typeof arguments[0] === 'object' ) {
-			options = arguments[0];
-		}
-		options = $.extend(defaults,options);
-		// Fetch
-		var $select = $(this).firstInput().hide().removeClass(options.hideClass);
-		var $list = $(options.list);
-		var $item = $(options.item);
-		var $title = $(options.title);
-		var $remove_button = $(options.remove_button);
-		var $clear = $(options.clear);
-		// Bind
-		$remove_button.click(function(e){
-			return options.remove.apply(this,[$select,e]);
-		});
-		if ( options.add_button && options.add_field ) {
-			var $add = $(options.add_button);
-			var $field = $(options.add_field);
-		}
-		// Build
-		$list.insertAfter($select);
-		$clear.insertAfter($list);
-		$select.children('option').each(function(){
-			var $option = $(this);
-			var title = $option.text();
-			var value = $option.value();
-			$title.text(title);
-			$item.clone().append($remove_button).append($title).data('value',value).attr('title',title).appendTo($list);
-		});
-	}
 	
-	// inlineEdit
-	$.fn.inlineEdit = function() {
-		// Prepare
-		var options = {};
-		var defaults = {
-			edit_button: '<a class="inline-edit-button inline-edit-button-edit">Edit</a>',
-			edit_button_class: '',
-			remove_button: '<a class="inline-edit-button inline-edit-button-remove">Remove</a>',
-			ok_button: '<a class="inline-edit-button inline-edit-button-ok button">OK</a>',
-			cancel_button: '<a class="inline-edit-button inline-edit-button-cancel">Cancel</a>',
-			panel: '<span class="inline-edit-panel"/>',
-			view_panel: '<span class="inline-edit-panel-view"/>',
-			edit_panel: '<span class="inline-edit-panel-edit"/>',
-			hideClass: 'hide',
-			highlightClass: 'editable',
-			clickableSelector: '.inline-edit-clickable,label',
-			open: function(els,e){
-				els.$edit.data('orig', els.$edit.val());
-				els.$views.hide();
-				els.$edits.show();
-				els.$edit.giveFocus();
-			},
-			update: function(els,e){
-				els.$view.html($edit.value());
-				els.$views.show();
-				els.$edits.hide();
-				return true;
-			},
-			cancel: function(els,e){
-				els.$edit.val(els.$edit.data('orig'));
-				els.$views.show();
-				els.$edits.hide();
-			},
-			remove: false
-		};
-		if ( typeof arguments[0] === 'string' ) {
-			options = {
-				edit: arguments[0]
-			};
-		} else if ( typeof arguments[0] === 'object' ) {
-			options = arguments[0];
-		}
-		if ( typeof arguments[1] === 'string' ) {
-			options.panel = arguments[1];
-		}
-		var appendPanel = typeof options.panel === 'undefined';
-		options = $.extend(defaults,options);
-		// Fetch
-		var $view = $(this).addClass(options.highlightClass);
-		var $edit = $(options.edit).hide().removeClass(options.hideClass);
-		var $edit_button = $(options.edit_button);
-		var $ok_button = $(options.ok_button);
-		var $cancel_button = $(options.cancel_button);
-		var $remove_button = $(options.remove_button);
-		var $panel = $(options.panel);
-		var $view_panel = $(options.view_panel);
-		var $edit_panel = $(options.edit_panel).hide();
-		// Handle
-		if ( options.edit_button_class && options.edit_button_class.length ) {
-			$edit_button.addClass(options.edit_button_class);
-		}
-		if ( !options.remove ) {
-			$remove_button.hide();
-		}
-		// Build
-		$view_panel.append($edit_button).append($remove_button);}
-		$edit_panel.append($ok_button).append($cancel_button);
-		$panel.append($view_panel).append($edit_panel);
-		$panel.insertAfter($edit);
-		// Simplify
-		var $views = $($view_panel).add($view);
-		var $edits = $($edit_panel).add($edit);
-		var $edit_buttons = $edit_button.add($view);
-		if ( options.clickableSelector ) {
-			$edit_buttons = $edit_buttons.add(
-				$view.add($edit).add($panel).siblings(options.clickableSelector)
-			);
-		}
-		// Functions
-		var pack = function(){
-			return {
-				$views: $views,
-				$edits: $edits,
-				$view: $view,
-				$edit: $edit,
-				$panel: $panel
-			}
-		}
-		var open = function(e){
-			options.open.apply(this,[pack(),e]);
-		};
-		var cancel = function(e){
-			options.cancel.apply(this,[pack(),e]);
-		};
-		var update = function(e){
-			options.update.apply(this,[pack(),e]);
-		};
-		var remove = function(e){
-			options.remove.apply(this,[pack(),e]);
-		};
-		// Bind
-		$edit_buttons.click(open);
-		$cancel_button.click(cancel); $edit.cancel(cancel);
-		$ok_button.click(update); $edit.enter(update);
-		if ( options.remove ) {
-			$remove_button.click(remove);
-		}
-		// Done
-		return this;
-	};
-	
-	// Editor
+	// BalClass
 	$.BalClass = function(config){
 		this.configure(config);
 	};
@@ -307,6 +156,114 @@
 			var Me = this;
 			return Me.getConfig('default',Me.getConfig(name,config));
 		}
+	};
+	
+	// InlineEdit
+	$.InlineEdit = new $.BalClass({
+		'default': {
+			edit_button: '<a class="inline-edit-button inline-edit-button-edit">Edit</a>',
+			edit_button_class: '',
+			remove_button: '<a class="inline-edit-button inline-edit-button-remove">Remove</a>',
+			ok_button: '<a class="inline-edit-button inline-edit-button-ok button">OK</a>',
+			cancel_button: '<a class="inline-edit-button inline-edit-button-cancel">Cancel</a>',
+			panel: '<span class="inline-edit-panel"/>',
+			view_panel: '<span class="inline-edit-panel-view"/>',
+			edit_panel: '<span class="inline-edit-panel-edit"/>',
+			hideClass: 'hide',
+			highlightClass: 'editable',
+			clickableSelector: '.inline-edit-clickable,label',
+			open: function(els,e){
+				els.$edit.data('orig', els.$edit.val());
+				els.$views.hide();
+				els.$edits.show();
+				els.$edit.giveFocus();
+			},
+			update: function(els,e){
+				els.$view.html($edit.value());
+				els.$views.show();
+				els.$edits.hide();
+				return true;
+			},
+			cancel: function(els,e){
+				els.$edit.val(els.$edit.data('orig'));
+				els.$views.show();
+				els.$edits.hide();
+			},
+			remove: false
+		},
+		'editbutton': {
+			edit_button_class: 'button'
+		}
+	});
+	$.fn.inlineEdit = function(option, options) {
+		// Prepare
+		var Me = $.InlineEdit;
+		var config = Me.getConfigWithDefault(option, options);
+		// Fetch
+		var $view = $(this).addClass(config.highlightClass);
+		var $edit = $(config.edit).hide().removeClass(config.hideClass);
+		var $edit_button = $(config.edit_button);
+		var $ok_button = $(config.ok_button);
+		var $cancel_button = $(config.cancel_button);
+		var $remove_button = $(config.remove_button);
+		var $panel = $(config.panel);
+		var $view_panel = $(config.view_panel);
+		var $edit_panel = $(config.edit_panel).hide();
+		// Handle
+		var insertPanel = !$panel.inDOM();
+		if ( config.edit_button_class && config.edit_button_class.length ) {
+			$edit_button.addClass(config.edit_button_class);
+		}
+		if ( !config.remove ) {
+			$remove_button.hide();
+		}
+		// Build
+		$view_panel.append($edit_button).append($remove_button);
+		$edit_panel.append($ok_button).append($cancel_button);
+		$panel.append($view_panel).append($edit_panel);
+		if ( insertPanel ) {
+			$panel.insertAfter($edit);
+		}
+		// Simplify
+		var $views = $($view_panel).add($view);
+		var $edits = $($edit_panel).add($edit);
+		var $edit_buttons = $edit_button.add($view);
+		if ( config.clickableSelector ) {
+			$edit_buttons = $edit_buttons.add(
+				$view.add($edit).add($panel).siblings(config.clickableSelector)
+			);
+		}
+		// Functions
+		var pack = function(){
+			return {
+				$views: $views,
+				$edits: $edits,
+				$view: $view,
+				$edit: $edit,
+				$panel: $panel
+			};
+		}
+		var open = function(e){
+			config.open.apply(this,[pack(),e]);
+		};
+		var cancel = function(e){
+			config.cancel.apply(this,[pack(),e]);
+		};
+		var update = function(e){
+			config.update.apply(this,[pack(),e]);
+		};
+		var remove = function(e){
+			config.remove.apply(this,[pack(),e]);
+		};
+		// Bind
+		$edit_buttons.click(open);
+		$cancel_button.click(cancel); $edit.cancel(cancel);
+		$ok_button.click(update); $edit.enter(update);
+		if ( config.remove ) {
+			$remove_button.click(remove);
+		}
+		// Done
+		return this;
 	};
 	
 	// Editor
@@ -362,9 +319,6 @@
 			parentClass: '',
 			title: ''
 		}
-	});
-	$.Help = $.extend($.Help,{
-		
 	});
 	$.fn.help = function(options){
 		var Me = $.Help;
