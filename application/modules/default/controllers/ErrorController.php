523331c78167979500a1adb6b1bd8232d933d100
Benjamin "balupton" Lupton <contact@balupton.com>
2010-02-05 06:48:54 +0000

    * Resource compat update on error controller

--- application/modules/default/controllers/ErrorController.php
+++ application/modules/default/controllers/ErrorController.php
@@ -21,7 +21,7 @@ class ErrorController extends Zend_Controller_Action {
 	 * @return
 	 */
     public function errorAction() {
-    	// Fetch
+    	# Fetch
 		$Registry = Zend_Registry::getInstance();
         $errors = $this->_getParam('error_handler');
 		$Exception = $errors->exception;
@@ -29,110 +29,56 @@ class ErrorController extends Zend_Controller_Action {
 		$env = $this->getInvokeArg('env');
 		$messages = array();
 		
-        // Use defined error layout
+		
+        # Use defined error layout
         $this->getHelper('layout')->disableLayout();
         
-        // Use defined error view
+        # Use defined error view
         $template = $this->getHelper('App')->getApp()->getConfig('bal.error.template');
         if ( $template ) {
             $this->_helper->viewRenderer($template);
         }
         
-		// Determine
-		switch ($errors->type) {
-		    case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
-		    case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
-				// 404 error -- controller or action not found
+		
+		# Fetch Exceptor
+		$Exceptor = new Bal_Exceptor($Exception);
+		
+		# Handle
+		$error = $Exceptor->getId();
+		switch ( $error ) {
+			case 'error-application-404':
 		        $this->getResponse()->setHttpResponseCode(404);
-		        $this->view->error = 'error-404';
-		        break;
+				break;
 			
-			case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_OTHER:
-				// Thrown but unkown, perhaps managed?
-				// Check if doctrine
-				$class = get_class($Exception);
-				switch ( $class ) {
-					case 'Doctrine_Connection_Mysql_Exception':
-						$this->view->error = $Exception->getPortableMessage();
-						break;
-					case 'Doctrine_Validator_Exception':
-						$this->view->error = 'error-doctrine-validation';
-						// Fetch
-						$invalidRecords = $Exception->getInvalidRecords();
-						// Cycle
-						foreach ( $invalidRecords as $Record ) {
-							// Fetch
-							$ErrorStack = $Record->getErrorStack()->toArray();
-							// Cycle
-							foreach ( $ErrorStack as $field => $errors ) {
-								foreach ( $errors as $error ) {
-									// Prepare
-									$Table = $Record->getTable();
-									$message = array(
-										'code' => 'error-doctrine-validation-'.$error,
-										'table' => strtolower($Table->getComponentName()),
-										'field' => $field,
-										'value' => $Record->get($field)
-									);
-									// Handle
-									switch ( $error ) {
-										case 'type':
-											$type = $Table->getTypeOf($field);
-											$message['type'] = $type;
-											break;
-										case 'length':
-											$properties = $Table->getDefinitionOf($field);
-											$message['length'] = $properties['length'];
-										default:
-											break;
-									}
-									// Append
-									$messages[] = $message;
-								}
-							}
-						}
-						break;
-					default:
-						break;
-				}
-				
-		    default:
-		        // application error
+			default:
 		        $this->getResponse()->setHttpResponseCode(500);
-				if ( empty($this->view->error) ) $this->view->error = 'error-application';
-		        break;
+				break;
 		}
 		
-		// Profiler
+		# Apply
+		$this->view->error = $error;
+		$this->view->title = $Exceptor->getTitle();
+		$this->view->messages = $Exceptor->getMessages();
+		$this->view->exceptor = $Exceptor->toString();
+		
+		
+		# Profiler
 		$Profiler = isset($Registery->Profiler) ? $Registery->Profiler : null;
         
-		// Assign
+		# Assign
         $this->view->env		= $env;
 		$this->view->exception 	= $Exception;
 		$this->view->request   	= $Request;
-		$this->view->messages	= $messages;
 		$this->view->profiler	= $Profiler;
 		
-		// Apply
+		# Apply
 		$this->view->headTitle()->append('Error');
 		$this->view->page = 'error';
 		
-		// Log the error
-		$Log = Zend_Registry::get('Log');
-		if ( $Log ) {
-			$Log->err($Exception);
-			$Log->info(var_export(array(
-				'server'	=> $_SERVER,
-				'get'		=> $_GET,
-				'post'		=> $_POST,
-				'session'	=> $_SESSION,
-				'cookie'	=> $_COOKIE,
-				'params' 	=> $_REQUEST,
-				'messages' 	=> $messages
-			),true));
-		}
+		# Log the error
+		$Exceptor->log();
 		
-		// Done
+		# Done
 		return true;
     }
 
