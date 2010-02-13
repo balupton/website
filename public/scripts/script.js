2aee36a00718aff30bd9516b20d682b7b892b32f
Benjamin "balupton" Lupton <contact@balupton.com>
2010-02-13 05:52:09 +0000

    * Applied HTML5 Doctype * Cleaned templates

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -32,7 +32,7 @@
 (function($){
 	
 	
-	// Log
+	// Hide Log Event Details
 	$(function(){
 		$('.log .event').click(function(){
 			$(this).find('.details').toggle();
@@ -101,6 +101,216 @@
 		$find.unbind('change',events.change).change(events.change).trigger('change');
 		// Done
 		return true;
+	}
+	
+	// Sparkle: delete warning
+	$.Sparkle.add('deletewarn', function(){
+		var $this = $(this); var Sparkle = $.Sparkle;
+		// Fetch
+		var $inputs = $this.findAndSelf('.delete-action').click(function(event){
+			var $this = $(this);
+			if ( !confirm($this.attr('title')||'Are you sure you want to delete this?') ) {
+				// Prevent
+				event.stopPropagation();
+				event.preventDefault();
+			}
+		});
+		// Done
+		return $this;
+	});
+	
+	// InlineEdit
+	$.InlineEdit = new $.BalClass({
+		'default': {
+			edit_button: '<a class="inline-edit-button inline-edit-button-edit">Edit</a>',
+			edit_button_class: '',
+			remove_button: '<a class="inline-edit-button inline-edit-button-remove">Remove</a>',
+			ok_button: '<a class="inline-edit-button inline-edit-button-ok button">OK</a>',
+			cancel_button: '<a class="inline-edit-button inline-edit-button-cancel">Cancel</a>',
+			panel: '<span class="inline-edit-panel"/>',
+			view_panel: '<span class="inline-edit-panel-view"/>',
+			edit_panel: '<span class="inline-edit-panel-edit"/>',
+			hideClass: 'hide',
+			highlightClass: 'editable',
+			clickableSelector: '.inline-edit-clickable,label',
+			open: function(e,els){
+				els.$edit.data('orig', els.$edit.val());
+				els.$views.hide();
+				els.$edits.show();
+				els.$edit.giveFocus();
+				return true;
+			},
+			update: function(e,els){
+				els.$view.html(els.$edit.value());
+				els.$views.show();
+				els.$edits.hide();
+				return true;
+			},
+			cancel: function(e,els){
+				els.$edit.val(els.$edit.data('orig'));
+				els.$views.show();
+				els.$edits.hide();
+				return true;
+			},
+			remove: false
+		},
+		'editbutton': {
+			edit_button_class: 'button'
+		}
+	});
+	$.fn.inlineEdit = function(option, options) {
+		// Prepare
+		var Me = $.InlineEdit;
+		var config = Me.getConfigWithDefault(option, options);
+		// Fetch
+		var $view = $(this).addClass(config.highlightClass);
+		var $edit = $(config.edit).hide().removeClass(config.hideClass);
+		var $edit_button = $(config.edit_button);
+		var $ok_button = $(config.ok_button);
+		var $cancel_button = $(config.cancel_button);
+		var $remove_button = $(config.remove_button);
+		var $panel = $(config.panel);
+		var $view_panel = $(config.view_panel);
+		var $edit_panel = $(config.edit_panel).hide();
+		// Handle
+		var insertPanel = !$panel.inDOM();
+		if ( config.edit_button_class && config.edit_button_class.length ) {
+			$edit_button.addClass(config.edit_button_class);
+		}
+		if ( !config.remove ) {
+			$remove_button.hide();
+		}
+		// Build
+		$view_panel.append($edit_button).append($remove_button);
+		$edit_panel.append($ok_button).append($cancel_button);
+		$panel.append($view_panel).append($edit_panel);
+		if ( insertPanel ) {
+			$panel.insertAfter($edit);
+		}
+		// Simplify
+		var $views = $($view_panel).add($view);
+		var $edits = $($edit_panel).add($edit);
+		var $edit_buttons = $edit_button.add($view);
+		if ( config.clickableSelector ) {
+			$edit_buttons = $edit_buttons.add(
+				$view.add($edit).add($panel).siblings(config.clickableSelector)
+			);
+		}
+		// Functions
+		var pack = function(){
+			return {
+				$views: $views,
+				$edits: $edits,
+				$view: $view,
+				$edit: $edit,
+				$panel: $panel
+			};
+		}
+		var open = function(e){
+			config.open.apply(this,[e,pack(),config]);
+		};
+		var cancel = function(e){
+			config.cancel.apply(this,[e,pack(),config]);
+		};
+		var update = function(e){
+			config.update.apply(this,[e,pack(),config]);
+		};
+		var remove = function(e){
+			config.remove.apply(this,[e,pack(),config]);
+		};
+		// Bind
+		$edit_buttons.click(open);
+		$cancel_button.click(cancel); $edit.cancel(cancel);
+		$ok_button.click(update); $edit.enter(update);
+		if ( config.remove ) {
+			$remove_button.click(remove);
+		}
+		// Done
+		return this;
+	};
+	
+	// Editor
+	$.Editor = new $.BalClass({
+		'default': {
+			// Location of TinyMCE script
+			script_url: '/scripts/tiny_mce/tiny_mce.js',
+			
+			// General options
+			theme: "advanced",
+			plugins: "autoresize,safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+
+			// Theme options
+			theme_advanced_buttons1: "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect,|,code,",
+			theme_advanced_buttons2: "cut,copy,paste,pastetext,pasteword,|,undo,redo,|,link,unlink,image,|,preview,|,forecolor,backcolor,|,bullist,numlist,|,outdent,indent,blockquote,|,fullscreen",
+			theme_advanced_buttons3: "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup",
+			theme_advanced_toolbar_location: "top",
+			theme_advanced_toolbar_align: "left",
+			theme_advanced_statusbar_location: "bottom",
+			theme_advanced_path: false,
+			theme_advanced_resizing: false,
+			width: "100%",
+			
+			// Compat
+			//add_form_submit_trigger: false,
+			//submit_patch: false,
+			
+			// Example content CSS (should be your site CSS)
+			// content_css : "css/content.css",
+			
+			// Replace values for the template plugin
+			template_replace_values: {
+				
+			}
+		},
+		'rich': {
+		},
+		'simple': {
+			theme_advanced_buttons2: "",
+			theme_advanced_buttons3: ""
+		}
+	});
+	$.fn.editor = function(options) {
+		var Me = $.Editor;
+		var config = Me.getConfigWithDefault(options,options);
+		var $this = $(this);
+		//$this.parent('form:first').unbind().submit(function(){
+			//tinyMCE.triggerSave();
+		//});
+		return $this.tinymce(config);
+	};
+	
+	// Help
+	$.Help = new $.BalClass({
+		'default': {
+			// Elements
+			wrap: '<span class="help-wrap"/>',
+			icon: '<span class="help-icon"/>',
+			text: '<span class="help-text"/>',
+			parentClass: '',
+			title: ''
+		}
+	});
+	$.fn.help = function(options){
+		var Me = $.Help;
+		if ( typeof options === 'string' ) {
+			options = {
+				title: options
+			};
+		}
+		var config = Me.getConfigWithDefault(options,options);
+		// Fetch
+		var $this = $(this);
+		var $wrap = $(config.wrap);
+		var $icon = $(config.icon);
+		var $text = $(config.text);
+		var $parent = $this.parent().addClass(config.parentClass);
+		// Build
+		var $contents = $this.contents();
+		$this.append($wrap.append($text).append($icon));
+		$contents.appendTo($text);
+		$this.attr('title', config.title);
+		// Done
+		return $this;
 	}
 	
 })(jQuery);
