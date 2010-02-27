820229826cd1cb904b401da168a93e922e694974
Benjamin "balupton" Lupton <contact@balupton.com>
2010-02-27 16:29:44 +0000

    * Updated misc to use DIRECTORY_SEPARATOR and attempted performance improvements

--- public/media/image.php
+++ public/media/image.php
@@ -1,12 +1,12 @@
 <?php
 # Init
 $bootstrap = $run = false;
-require_once (dirname(__FILE__) . '/../../index.php');
+require_once (dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'index.php');
 $Application->bootstrap('balphp');
 
 # Requires
-require_once (BALPHP_PATH . '/core/functions/_image.funcs.php');
-require_once (BALPHP_PATH . '/core/functions/_files.funcs.php');
+require_once (BALPHP_PATH . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR . '_image.funcs.php');
+require_once (BALPHP_PATH . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR . '_files.funcs.php');
 
 # Fetch Paths
 $media_path		= Bal_App::getConfig('media_path');
@@ -15,7 +15,7 @@ $images_path	= Bal_App::getConfig('images_path');
 $images_url		= Bal_App::getConfig('images_url');
 
 # Fetch
-$image_location = ltrim($_GET['image'], '/');
+$image_location = ltrim($_GET['image'], DIRECTORY_SEPARATOR);
 $height = intval(!empty($_GET['height']) ? $_GET['height'] : (!empty($_GET['h']) ? $_GET['h'] : 0));
 $width = intval(!empty($_GET['width']) ? $_GET['width'] : (!empty($_GET['w']) ? $_GET['w'] : 0));
 $quality = intval(!empty($_GET['quality']) ? $_GET['quality'] : (!empty($_GET['q']) ? $_GET['q'] : 90));
