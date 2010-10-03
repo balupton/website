936eca7eb8bfb0e019fd15816d8930e9dcff2aec
Benjamin "balupton" Lupton <contact@balupton.com>
2010-10-03 19:35:36 +0800

    Updated config.php for balupton.com servers

--- config.php
+++ config.php
@@ -13,13 +13,20 @@ if ( strpos($_SERVER['DOCUMENT_ROOT'], '/Users/balupton/Server/public_html') !==
 	define('APPLICATION_ENV', 				'development');
 	define('COMMON_PATH', 					'/Users/balupton/Server/public_html/common');
 }
+elseif ( $_SERVER['DOCUMENT_ROOT'] === '/home/balupton/subdomains/testing.balupton.com' ||  $_SERVER['SERVER_NAME'] === 'testing.balupton.com' ) {
+	# Testing Server
+	define('APPLICATION_ENV', 				'testing');
+	define('COMMON_PATH', 					'/home/balupton/common');
+}
 elseif ( $_SERVER['DOCUMENT_ROOT'] === '/home/balupton/subdomains/staging.balupton.com' ||  $_SERVER['SERVER_NAME'] === 'staging.balupton.com' ) {
 	# Staging Server
 	define('APPLICATION_ENV', 				'staging');
+	define('COMMON_PATH', 					'/home/balupton/common');
 }
 elseif ( $_SERVER['DOCUMENT_ROOT'] === '/home/balupton/public_html' || strpos($_SERVER['HTTP_HOST'], 'balupton.com') !== false ) {
 	# Production Server
 	define('APPLICATION_ENV', 				'production');
+	define('COMMON_PATH', 					'/home/balupton/common');
 }
 elseif ( true ) {
 	# Uncofigured Environment
