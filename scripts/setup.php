4f365a11a745b183329baa66ea897b20a87a9a1e
Benjamin "balupton" Lupton <contact@balupton.com>
2010-12-03 05:20:19 +1100

    Updated scaffold submodule. Update configure and setup with stability improvements.

--- scripts/setup.php
+++ scripts/setup.php
@@ -9,6 +9,11 @@ if ( empty($GLOBALS['Application']) ) {
 # Load
 $GLOBALS['Application']->bootstrap('ScriptSetup');
 
+# Prepare
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 # App
 Bal_App::getInstance($Application)->setup();
 die;
