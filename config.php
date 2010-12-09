7742c50c6e0d6bc087df10f315dd9017a256a72e
Benjamin "balupton" Lupton <contact@balupton.com>
2010-12-10 02:28:20 +0800

    Updated for staging server.

--- config.php
+++ config.php
@@ -4,60 +4,68 @@
  * This file is used to set our necessary basic configuration, mainly the application environment we are using.
  */
 
-# --------------------------
+# ==========================
 # Adjust Application Environments
 
-# Include paths
-if ( strpos(strtolower($_SERVER['DOCUMENT_ROOT']), '/users/balupton/dropbox/server/public_html') !== false ) {
-	# Development Environment
-	define('APPLICATION_ENV', 				'development');
-	$_SERVER['HTTP_HOST'] = 				'localhost';
-}
-elseif ( $_SERVER['DOCUMENT_ROOT'] === '/home/balupton/subdomains/testing.balupton.com' ||  $_SERVER['SERVER_NAME'] === 'testing.balupton.com' ) {
-	# Testing Server
-	define('APPLICATION_ENV', 				'testing');
-	$_SERVER['HTTP_HOST'] = 				'localhost';
-}
-elseif ( $_SERVER['DOCUMENT_ROOT'] === '/home/balupton/subdomains/staging.balupton.com' ||  $_SERVER['SERVER_NAME'] === 'staging.balupton.com' ) {
-	# Staging Server
-	define('APPLICATION_ENV', 				'staging');
-	$_SERVER['HTTP_HOST'] = 				'localhost';
-}
-elseif ( $_SERVER['DOCUMENT_ROOT'] === '/home/balupton/public_html' ) {
-	# Production Server
-	define('APPLICATION_ENV', 				'production');
-	$_SERVER['HTTP_HOST'] = 				'balupton.com';
-}
-elseif
-(
-	strpos($_SERVER['HTTP_HOST'], 'balupton.com') !== false
-) {
-	# Production Server
-	define('APPLICATION_ENV', 				'production');
-	$_SERVER['HTTP_HOST'] = 				'balupton.com';
-}
-elseif
-(
-		$_SERVER['DOCUMENT_ROOT'] === '/nfs/c07/h02/mnt/113264/domains/balupton.com/html'
+# --------------------------
+# Production Server
+if (
+	 	strpos($_SERVER['HTTP_HOST'], 'balupton.com') !== false
+	|| 	strpos($_SERVER['HTTP_HOST'], 'balupton.com.s113264.gridserver.com') !== false
+	||	$_SERVER['DOCUMENT_ROOT'] === '/nfs/c07/h02/mnt/113264/domains/balupton.com/html'
 	|| 	$_SERVER['DOCUMENT_ROOT'] === '/home/113264/users/.home/domains/balupton.com/html'
 	|| 	$_SERVER['DOCUMENT_ROOT'] === '/home/113264/domains/s113264.gridserver.com/html'
 	|| 	$_SERVER['DOCUMENT_ROOT'] === '/home/113264/domains/balupton.com/html'
-	|| 	strpos($_SERVER['HTTP_HOST'], 'balupton.com.s113264.gridserver.com') !== false
-	|| 	strpos($_SERVER['HTTP_HOST'], 'balupton.com') !== false
 ) {
-	# Production Server
+	# Prepare
+	$preview_host = 'balupton.com.s113264.gridserver.com';
+	$live_host = 'balupton.com';
+
+	# Apply
 	define('APPLICATION_ENV', 				'production');
-	define('HTTP_HOST',						'balupton.com.s113264.gridserver.com');
-	define('ROOT_URL',						'http://balupton.com.s113264.gridserver.com');
-	$_SERVER['HTTP_HOST'] = 				'balupton.com.s113264.gridserver.com';
+	$_SERVER['HTTP_HOST'] = 				$live_host;
 }
-elseif ( false ) {
-	# Uncofigured Environment
+
+# --------------------------
+# Staging Server
+elseif (
+	 	strpos($_SERVER['HTTP_HOST'], 'staging.balupton.com') !== false
+	||	strpos($_SERVER['HTTP_HOST'], 'staging.balupton.com.s113264.gridserver.com') !== false
+	||	$_SERVER['DOCUMENT_ROOT'] === '/nfs/c07/h02/mnt/113264/domains/staging.balupton.com/html'
+	|| 	$_SERVER['DOCUMENT_ROOT'] === '/home/113264/users/.home/domains/staging.balupton.com/html'
+	|| 	$_SERVER['DOCUMENT_ROOT'] === '/home/113264/domains/staging.balupton.com/html'
+) {
+	# Prepare
+	$preview_host = 'staging.balupton.com.s113264.gridserver.com';
+	$live_host = 'staging.balupton.com';
+
+	# Apply
+	define('APPLICATION_ENV', 				'staging');
+	$_SERVER['HTTP_HOST'] = 				$preview_host;
+}
+
+# --------------------------
+# Testing Server
+elseif (
+		$_SERVER['DOCUMENT_ROOT'] === '/home/balupton/subdomains/testing.balupton.com'
+	||  $_SERVER['SERVER_NAME'] === 'testing.balupton.com'
+) {
+	define('APPLICATION_ENV', 				'testing');
+	$_SERVER['HTTP_HOST'] = 				'localhost';
+}
+
+# --------------------------
+# Development Environment
+elseif (
+		strpos(strtolower($_SERVER['DOCUMENT_ROOT']), '/users/balupton/dropbox/server/public_html') !== false
+) {
 	define('APPLICATION_ENV', 				'development');
 	$_SERVER['HTTP_HOST'] = 				'localhost';
 }
+
+# --------------------------
+# Unconfigured Environment
 else {
-	# Uncofigured Environment
 	throw new Exception('Unknown Project Location');
 }
 
